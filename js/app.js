b158d5a7cdd2a6800715721a7399667f77125282
rodjona <rodjon@gmail.com>
2026-07-08 13:45:42 -0400

    Delete js directory

--- js/app.js
+++
@@ -1,4 +0,0 @@
-const menuButton=document.querySelector('.menu');
-const navLinks=document.querySelector('.navlinks');
-if(menuButton&&navLinks){menuButton.addEventListener('click',()=>navLinks.classList.toggle('open'));}
-document.querySelectorAll('.navlinks a').forEach(a=>a.addEventListener('click',()=>navLinks?.classList.remove('open')));
